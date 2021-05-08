bf1dceb16dd7da6a1c4d4c2fc9b3b5c04eecf31c
Tatsuhiko Kimita <nannbo@crappo.net>
2021-05-08 16:49:08 +0900

    手直しコミット

--- src/main/java/page/ClapAndWhistle/forkapp/App.java
+++ src/main/java/page/ClapAndWhistle/forkapp/App.java
@@ -1,10 +1,10 @@
-package page.clap-and-whistle.forkapp;
+package page.ClapAndWhistle.forkapp;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
